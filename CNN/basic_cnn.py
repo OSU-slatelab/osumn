95eb24edbfccdc8d7a3980226fef5021bb8397c6
Peter Plantinga <plantinga.peter@protonmail.com>
2017-12-05 12:39:38 -0500

    Update cnn file with most recent ResNet-style architecture

--- CNN/basic_cnn.py
+++ CNN/basic_cnn.py
@@ -6,9 +6,9 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from data_io import DataLoader
 
+
 def batch_norm(x, shape, training, decay = 0.999, epsilon = 1e-3):
     """ Batch Norm for controlling batch statistics """
-    #Assume 2d [batch, values] tensor
     beta = tf.get_variable(name='beta', shape=shape, initializer=tf.constant_initializer(0.0)
                                , trainable=True)
     gamma = tf.get_variable(name='gamma', shape=shape, initializer=tf.random_normal_initializer(1.0, 0.02),
@@ -21,7 +21,7 @@ def batch_norm(x, shape, training, decay = 0.999, epsilon = 1e-3):
                               shape,
                               initializer=tf.constant_initializer(1.0),
                               trainable=False)
-    batch_mean, batch_var = tf.nn.moments(x, [0])
+    batch_mean, batch_var = tf.nn.moments(x, [0,1,2])
 
     train_mean_op = tf.assign(pop_mean, pop_mean * decay + batch_mean * (1 - decay))
     train_var_op = tf.assign(pop_var, pop_var * decay + batch_var * (1 - decay))
@@ -35,15 +35,58 @@ def batch_norm(x, shape, training, decay = 0.999, epsilon = 1e-3):
 
     return tf.cond(training, batch_statistics, population_statistics)
 
+def conv_block(inputs, filters, training, dropout):
+    reduced = tf.layers.conv2d(
+        inputs      = inputs,
+        filters     = filters,
+        kernel_size = 3,
+        strides     = (2,2),
+        padding     = 'same',
+        kernel_regularizer = tf.contrib.layers.l2_regularizer(0.0001),
+    )
+
+    shape = tf.shape(reduced)
+    reduced = tf.layers.dropout(reduced, rate=dropout, noise_shape=[shape[0], 1, 1, filters], training=training)
+
+    with tf.variable_scope("conv1"):
+
+        conv1 = tf.nn.relu(reduced)
+        conv1 = tf.layers.conv2d(
+            inputs      = conv1,
+            filters     = filters,
+            kernel_size = 3,
+            padding     = 'same',
+            activation  = tf.nn.relu,
+            kernel_regularizer = tf.contrib.layers.l2_regularizer(0.0001),
+        )
+        conv1 = tf.layers.dropout(conv1, rate=dropout, noise_shape=[shape[0], 1, 1, filters], training=training)
+
+    with tf.variable_scope("conv2"):
+
+        conv2 = tf.layers.conv2d(
+            inputs      = conv1,
+            filters     = filters,
+            kernel_size = 3,
+            padding     = 'same',
+            activation  = tf.nn.relu,
+            kernel_regularizer = tf.contrib.layers.l2_regularizer(0.0001),
+        )
+        conv1 = tf.layers.dropout(conv1, rate=dropout, noise_shape=[shape[0], 1, 1, filters], training=training)
+        
+    out = conv2 + reduced
+
+    return out
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--base_dir", default="/fs/project/PAS1315/group1_chime2_data")
     parser.add_argument("--units", default=2048)
     parser.add_argument("--layers", default=2)
     parser.add_argument("--dropout", default=0.3)
-    parser.add_argument("--batch_size", default=1024)
-    parser.add_argument("--buffer_size", default=10)
-    parser.add_argument("--context", default=5)
+    parser.add_argument("--batch_size", default=256)
+    parser.add_argument("--buffer_size", default=40)
+    parser.add_argument("--context", default=10)
+    parser.add_argument("--checkpoints", default="checkpoints")
     args = parser.parse_args()
 
     train_loader = DataLoader(
@@ -68,73 +111,62 @@ if __name__ == "__main__":
         shuffle = False,
     )
 
-
-    in_frames = tf.placeholder(tf.float32, shape=(None, 2*args.context + 1, 771))
+    in_frames = tf.placeholder(tf.float32, shape=(None, 2*args.context + 1, 257))
     out_frames = tf.placeholder(tf.float32, shape=(None, 257))
     training = tf.placeholder(tf.bool)
 
-    conv_in = tf.reshape(in_frames, shape=(-1, 2*args.context+1, 257, 3))
-    conv1 = tf.layers.conv2d(
-        inputs      = conv_in,
-        filters     = 32,
-        kernel_size = 7,
-        padding     = 'same',
-        activation  = tf.nn.relu,
-    )
-    conv1 = tf.layers.max_pooling2d(
-        inputs      = conv1,
-        pool_size   = 2,
-        strides     = 2,
-        padding     = 'same',
-    )
-    conv2 = tf.layers.conv2d(
-        inputs      = conv1,
-        filters     = 64,
-        kernel_size = 3,
-        padding     = 'same',
-        activation  = tf.nn.relu,
-    )
-    conv2 = tf.layers.max_pooling2d(
-        inputs      = conv2,
-        pool_size   = 2,
-        strides     = 2,
-        padding     = 'same',
-    )
-    conv3 = tf.layers.conv2d(
-        inputs      = conv2,
-        filters     = 96,
-        kernel_size = 3,
-        padding     = 'same',
-        activation  = tf.nn.relu,
-    )
-    cnn_out = tf.contrib.layers.flatten(conv3)
-    fc1 = tf.layers.dense(cnn_out, 2048, tf.nn.relu)
-    fc1 = batch_norm(fc1, 2048, training)
-    fc1 = tf.layers.dropout(fc1, training=training)
-    out = tf.layers.dense(fc1, 257)
+    block = tf.reshape(in_frames, shape=(-1, 2*args.context+1, 257, 1))
+
+    filters = [128, 128, 256, 256]
+    for i, f in enumerate(filters):
+        with tf.variable_scope("block{0}".format(i)):
+            block = conv_block(block, f, training, args.dropout)
+
+    fc = tf.contrib.layers.flatten(block)
+    fc = tf.layers.dropout(fc, rate=args.dropout, training=training)
+
+    for i in range(args.layers):
+        with tf.variable_scope("fc{0}".format(i)):
+            fc = tf.layers.dense(fc, args.units, tf.nn.relu)
+            fc = tf.layers.dropout(fc, rate=args.dropout, training=training)
+
+    out = tf.layers.dense(fc, 257)
 
     loss = tf.losses.mean_squared_error(out_frames, out)
     global_step = tf.Variable(0, trainable=False)
-    learning_rate = tf.train.exponential_decay(0.001, global_step, 1e4, 0.95)
+    learning_rate = tf.train.exponential_decay(0.0001, global_step, 1e4, 0.95)
     train = tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step=global_step)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
-        for epoch in range(100):
+        saver = tf.train.Saver()
+
+        min_loss = 1
+        for epoch in range(0, 100):
             print("Epoch", epoch)
 
             train_loss = 0
-            for in_frame_batch, out_frame_batch in train_loader.batchify():
+            count = 0
+            for in_frame_batch, out_frame_batch in train_loader.batchify(shuffle_batches=False,include_deltas=False):
                 fd = {in_frames: in_frame_batch, out_frames: out_frame_batch, training: True}
                 batch_loss, _ = sess.run([loss, train], fd)
                 train_loss += batch_loss
+                count += 1
+                print("\r{0}/{1} - loss: {2}".format(count,5300*1024//args.batch_size,train_loss/count), end='', flush=True)
 
-            print("Train loss:", train_loss / 5300)
+            print("\nTrain loss:", train_loss / count)
 
             test_loss = 0
-            for in_frame_batch, out_frame_batch in dev_loader.batchify():
+            count = 0
+            for in_frame_batch, out_frame_batch in dev_loader.batchify(include_deltas=False):
                 fd = {in_frames: in_frame_batch, out_frames: out_frame_batch, training: False}
                 test_loss += sess.run(loss, fd)
+                count += 1
 
-            print("Test loss:", test_loss / 1612)
+            test_loss = test_loss / count
+
+            print("Test loss:", test_loss)
+
+            if test_loss < min_loss:
+                saver.save(sess, os.path.join(args.checkpoints, "model-{0}.ckpt".format(test_loss)))
